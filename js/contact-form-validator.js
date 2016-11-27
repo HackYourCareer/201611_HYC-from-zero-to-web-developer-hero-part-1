64a7d3f01dcf904730584386c8f05c1a035b2123
kkamil <kamil.kupczynski@hybris.com>
2016-11-27 16:43:11 +0100

    email sending added

--- js/contact-form-validator.js
+++ js/contact-form-validator.js
@@ -1,24 +1,43 @@
-$(document).ready(function () {
-
-    $('#cf-submit').click(function () {
-        console.log('validating!')
+$(document).ready(function() {
+    $('#cf-submit').click(function() {
         let email = $('#cf-email').val();
         let content = $('#cf-content').val();
-        
-        let isValidEmail = validateEmail(email);
-        let isValidContent = validateContent(content);
 
-        
+        let isValidEmail = validateEmail(email);
+
+        if (isValidEmail) {
+            sendEmail(email, content, addConfirmationMessage);
+        }
+        else {
+            addErrorMessage();
+        }
+        return false;
     });
 });
 
 function validateEmail(email) {
-    let hasAtSign = email.indexOf('@') !== -1;
     let emailRegexp = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
     return email.match(emailRegexp);
 }
 
-function validateContent() {
+function addConfirmationMessage() {
+
+}
+
+function addErrorMessage() {
     
+}
+
+function sendEmail(email, content, callback) {
+    $.ajax({
+        method: 'POST',
+        url: 'http://localhost:2403/emaile',
+        data: {
+            nadawca: email,
+            tresc: content
+        }
+    }).done(function(response) {
+        callback();
+    })
 }
