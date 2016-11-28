053093a5f06933b1e18758ac4ceec6cbba53ddcb
kkamil <kamil.kupczynski@hybris.com>
2016-11-29 00:57:05 +0100

    JS, STEP 6

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
-    //init dynamic content
-    $('#beer-tabs').tabs(); //jQuery ui plugin
+    //obsługuje kliknięcia na elementach z klasą a-checkbox--primary
+    $('.a-checkbox--primary').click(function() {
+        let allCheckedOptions = getAllCheckedOptions();
+        let filterObject = composeFilterObject(allCheckedOptions);
 
-    //active tab colouring
+        if (!$.isEmptyObject(filterObject)) {
+            beerStorage.getFilteredBeers(filterObject, appendBeerDivs);
+        } else {
+            beerStorage.getAllBeers(appendBeerDivs);
+        }
+    });
+
+    $('#beer-tabs').tabs(); //jQuery ui plugin, nie zaprzątajmy sobie tym głowy
+
+    //kolorowanie aktywnej zakładki
     $('.m-filter__item .a-btn').click(function() {
         const $this = $(this);
         const $allButtons = $('.m-filter__item .a-btn');
@@ -11,3 +22,40 @@ $(document).ready(function() {
         $this.addClass('a-btn--is-active');
     });
 });
+
+//nie będziemy patrzeć co tu się dzieje - nic waznego ;)
+function getAllCheckedOptions() {
+    let allCheckboxes = $('#beer-tabs').find('.a-checkbox--primary');
+    let checked = [];
+
+    allCheckboxes.each(function(iterator, checkboxElem) {
+        if (checkboxElem.checked) checked.push(checkboxElem);
+    });
+
+    return checked;
+}
+
+function composeFilterObject(checkedOptions) {
+    let filterObject = {};
+    checkedOptions.forEach(function(checkedElem) {
+        let [option, value] = checkedElem.value.split('-');
+        if (!isNaN(value)) value = +value;
+        if (filterObject[option]) {
+            filterObject[option].$in.push(value);
+        } else {
+            filterObject[option] = { $in: [value] };
+        }
+    });
+    return filterObject;
+}
+
+function grabBeerTemplate(callback) {
+    $.ajax({
+        url: '../templates/beer-template.hbs',
+        cache: true
+    })
+    .done(function(source) {
+         beerTemplate = Handlebars.compile($(source).html());
+         callback();
+    });
+}
