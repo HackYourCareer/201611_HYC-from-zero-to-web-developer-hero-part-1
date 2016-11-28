d06d6f35664983501d5fce6a7310345199cd5c1e
kkamil <kamil.kupczynski@hybris.com>
2016-11-28 23:24:41 +0100

    JS, STEP2

--- js/contact-form-validator.js
+++ js/contact-form-validator.js
@@ -1,3 +1,15 @@
-$(document).ready(function() { //czekamy, az dokument html zostanie załadowany
+$(document).ready(function() {
+    $('#cf-submit').click(function() { //podpinamy się pod element html o ID 'cf-submit', reagujemy na 'click event'
+        let email = $('#cf-email').val();
+        let content = $('#cf-content').val();
 
+        let isValidEmail = validateEmail(email);
+        let isContentFilled = content.length > 0; 
+    });
 });
+
+function validateEmail(email) { //deklaracja funkcji validateEmail
+    let emailRegexp = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+
+    return email.match(emailRegexp);
+}
