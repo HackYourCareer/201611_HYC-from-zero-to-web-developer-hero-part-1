b59dc6ed67709c7b6c873f622c6d0a456dbba9af
kkamil <kamil.kupczynski@hybris.com>
2016-11-28 23:52:26 +0100

    JS, STEP 3

--- js/contact-form-validator.js
+++ js/contact-form-validator.js
@@ -1,15 +1,58 @@
 $(document).ready(function() {
-    $('#cf-submit').click(function() { //podpinamy się pod element html o ID 'cf-submit', reagujemy na 'click event'
+    $('#cf-submit').click(function() {
         let email = $('#cf-email').val();
         let content = $('#cf-content').val();
 
         let isValidEmail = validateEmail(email);
         let isContentFilled = content.length > 0; 
+
+        if (isValidEmail && isContentFilled) {   //wysyłamy email...
+            sendEmail(email, content, addConfirmationMessage); 
+        }
+        else { //...lub pokazujemy błąd
+            if (!isValidEmail) {
+                addErrorMessage('Podaj prawidłowy adres email!');
+            }
+            else {
+                addErrorMessage('Hej, wysil się trochę!');
+            }
+        }
+        return false;
+    });
+
+    $('#cf-email, #cf-content').click(function() {
+        removeSubmissionInfo();
     });
 });
 
-function validateEmail(email) { //deklaracja funkcji validateEmail
+function validateEmail(email) {
     let emailRegexp = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
     return email.match(emailRegexp);
+}
+
+function addConfirmationMessage() {
+    const confirmMsg = 'Twoja wiadomość poleciała do Andrzeja!'
+    $('#submission-info').text(confirmMsg);
+}
+
+function addErrorMessage(errorMsg) {
+    $('#submission-info').text(errorMsg);
+}
+
+function removeSubmissionInfo() {
+    $('#submission-info').text('');
+}
+
+function sendEmail(email, content, callback) {
+    $.ajax({   //żądanie do serwera, bez przeładowania strony
+        method: 'POST',
+        url: 'http://localhost:2403/emaile',
+        data: {
+            nadawca: email,
+            tresc: content
+        }
+    }).done(function(response) {
+        callback();
+    })
 }
